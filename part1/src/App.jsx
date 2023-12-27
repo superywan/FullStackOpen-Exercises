6212ef951b5a663ddd0bbd865a1b1bcb65e95a8f
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2023-12-28 05:30:24 +0900

    1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -41,24 +41,26 @@ const Total = props => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content exercises={[part1, part2, part3]} />
-      <Total exercises={[part1, part2, part3]} />
+      <Content exercises={parts} />
+      <Total exercises={parts} />
     </div>
   );
 };
