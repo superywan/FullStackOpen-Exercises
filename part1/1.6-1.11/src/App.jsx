c30f9f76075049de0947fdf395ddc4c01f63835b
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2024-01-09 16:16:32 +0900

    1.9

--- part1/1.6-1.11/src/App.jsx
+++ part1/1.6-1.11/src/App.jsx
@@ -5,6 +5,15 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good * 1 + bad * -1) / total;
   const positiveAverage = (good / total) * 100;
 
+  if (total == 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>statistics</h1>
