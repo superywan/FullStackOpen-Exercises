c75f545e2b50aa69415544e5382ec50f1d97338d
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2024-02-11 21:21:16 +0900

    2.5

--- part2/2.1-2.5/src/App.jsx
+++ part2/2.1-2.5/src/App.jsx
@@ -1,44 +1,4 @@
-const Part = ({ part }) => {
-  return (
-    <p id={part.id}>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  const total = parts.reduce((acc, current) => acc + current.exercises, 0);
-
-  return (
-    <div>
-      {parts.map(part => (
-        <Part part={part} />
-      ))}
-      <p>
-        <strong>total of {total} exercises</strong>
-      </p>
-    </div>
-  );
-};
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map(course => {
-        return (
-          <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
