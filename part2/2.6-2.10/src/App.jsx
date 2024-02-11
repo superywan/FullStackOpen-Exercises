d276fc4b81c12a440a4ab0d8c7ecece8ec0f6499
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2024-02-12 01:09:53 +0900

    2.6

---
+++ part2/2.6-2.10/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newPersons = [...persons, { name: newName }];
+    setPersons(newPersons);
+    setNewName("");
+  };
+
+  const handleNewNameChange = event => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <div id={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
