2dd585fd4afe7718e4aa93691161ddecd6486077
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2024-02-19 22:56:40 +0900

    2.20

--- part2/2.18-2.20/src/App.jsx
+++ part2/2.18-2.20/src/App.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 function App() {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState("")
+  const [temperature, setTemperature] = useState(undefined)
 
   useEffect(() => {
     axios
@@ -13,16 +15,75 @@ function App() {
       })
   }, [])
 
-  const handleSearchChange = e => {
-    setSearch(e.target.value)
-  }
-  const handleCountryShow = countryName => {
-    setSearch(countryName)
-  }
-
   const filteredCountries = countries.filter(country =>
     country.name.common.toUpperCase().includes(search.toUpperCase())
   )
+
+  const getWeather = country => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${country.latlng[0]}&lon=${country.latlng[1]}&units=metric&appid=${API_KEY}`
+      )
+      .then(response => {
+        console.log(response)
+        setTemperature(response.data)
+      })
+  }
+
+  const handleSearchChange = e => {
+    setSearch(e.target.value)
+    const filtered = countries.filter(country =>
+      country.name.common.toUpperCase().includes(e.target.value.toUpperCase())
+    )
+    if (filtered.length === 1) {
+      console.log("hello!")
+      getWeather(filtered[0])
+    }
+  }
+  const handleCountryShow = countryName => {
+    setSearch(countryName)
+    const filtered = countries.filter(country =>
+      country.name.common.toUpperCase().includes(countryName.toUpperCase())
+    )
+    if (filtered.length === 1) {
+      console.log("hello!")
+      getWeather(filtered[0])
+    }
+  }
+
+  if (filteredCountries.length === 1) {
+    return (
+      <div>
+        <h1>{filteredCountries[0].name.common}</h1>
+        <div>capital {filteredCountries[0].capital[0]}</div>
+        <div>area {filteredCountries[0].area}</div>
+        <h3>languages:</h3>
+        <ul>
+          {Object.entries(filteredCountries[0].languages).map(([, value]) => {
+            return <li key={value}>{value}</li>
+          })}
+        </ul>
+        <img
+          src={filteredCountries[0].flags.png}
+          alt={filteredCountries[0].flags.alt}
+        />
+        {temperature ? (
+          <div>
+            <h2>Weather in {filteredCountries[0].name.common}</h2>
+            <div>temperature {temperature.current.temp} Celcius</div>
+            <img
+              src={`https://openweathermap.org/img/wn/${temperature.current.weather[0].icon}@2x.png`}
+              alt={`temperature ${temperature.current.temp}`}
+            />
+            <div>wind {temperature.current.wind_speed} m/s</div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    )
+  }
+
   return (
     <div>
       <div>
@@ -34,24 +95,6 @@ function App() {
         />
         {filteredCountries.length > 10 ? (
           <div>Too many matches, specify another filter</div>
-        ) : filteredCountries.length === 1 ? (
-          <div>
-            <h1>{filteredCountries[0].name.common}</h1>
-            <div>capital {filteredCountries[0].capital[0]}</div>
-            <div>capital {filteredCountries[0].area}</div>
-            <h3>languages:</h3>
-            <ul>
-              {Object.entries(filteredCountries[0].languages).map(
-                ([, value]) => {
-                  return <li key={value}>{value}</li>
-                }
-              )}
-            </ul>
-            <img
-              src={filteredCountries[0].flags.png}
-              alt={filteredCountries[0].flags.alt}
-            />
-          </div>
         ) : (
           filteredCountries.map(country => {
             return (
