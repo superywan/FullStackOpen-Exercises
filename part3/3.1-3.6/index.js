293b24ea24230df414d65e8064a72516f5bf5bf5
Wan Eddy Yi <waneddyyi@waaaaaanmacbookpro.local>
2024-02-25 21:12:44 +0900

    3.8

--- part3/3.1-3.6/index.js
+++ part3/3.1-3.6/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 
+const morgan = require('morgan')
+morgan.token("body", (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
